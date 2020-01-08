22bc910b8ab0ba9df020adbea181813dd15e9f1a
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-01-08 20:32:24 +0700

    add products api

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -7,10 +9,16 @@ router.get('/', (req, res, next) => {
     });
  });
  
- router.get('/id', (req, res, next) => {
-     res.send({
-         productId : 'test'
-     });
-  });
+ router.get('/:id', (req, res, next) => {
+
+    const id = req.params.id;
+
+    fs.readFile('./resources/products.json', 'utf-8', (err, data) => {
+        const products = JSON.parse(data);
+        const product = products[id];
+        res.send(product);
+    });
+
+ });
  
  module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -7,10 +9,16 @@ router.get('/', (req, res, next) => {
    });
 });
 
-router.get('/id', (req, res, next) => {
-    res.send({
-        userId : 'test'
+router.get('/:id', (req, res, next) => {
+
+    const id = req.params.id;
+
+    fs.readFile('./resources/users.json', 'utf-8', (err, data) => {
+        const users = JSON.parse(data);
+        const user = users[id];
+        res.send(user);
     });
+
  });
 
 module.exports = router;
